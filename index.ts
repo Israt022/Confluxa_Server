0d23a0c1923fc72efcd2c4d5dfa2bbf7c383a29e
Israt022 <eraisrat022@gmail.com>
2026-07-13 21:49:43 +0600

    User get post

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-import { MongoClient } from 'mongodb';
+import { MongoClient, ObjectId } from 'mongodb';
 import { createRemoteJWKSet, JWTPayload, jwtVerify } from "jose-cjs";
 const client = new MongoClient(process.env.MONGODB_URI as string);
 
@@ -31,59 +31,59 @@ const verifyToken = async (
 ) => {
   const authHeader = req.headers.authorization;
   console.log(authHeader)
-  if(!authHeader || !authHeader.startsWith('Bearer')){
-    return res.status(401).json({message : "Unauthorize"});
+  if (!authHeader || !authHeader.startsWith('Bearer')) {
+    return res.status(401).json({ message: "Unauthorize" });
   }
 
   const token = authHeader.split(" ")[1];
-  
-  if(!token){
-    return res.status(401).json({message : "Unauthorize"});
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorize" });
   }
 
-  try{
-    const {payload} = await jwtVerify(token, JWKS);
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
     // console.log(payload);
     // req.user = payload;
-    (req as any).user = payload;    
+    (req as any).user = payload;
     next();
-  }catch(error){
+  } catch (error) {
     // console.log(error)
-    return res.status(401).json({message : "Unauthorize"});
+    return res.status(401).json({ message: "Unauthorize" });
   }
 }
 
 // Organizer verify
-const organizerVerify = async(req: Request,
+const organizerVerify = async (req: Request,
   res: Response,
   next: NextFunction) => {
   const user = (req as AuthRequest).user;
 
-  
-  if(user?.role !== "organizer"){
-    return res.status(403).json({message : "Forbidden"});
+
+  if (user?.role !== "organizer") {
+    return res.status(403).json({ message: "Forbidden" });
   }
 
   next();
 };
 // User verify
-const userVerify = async(req: Request,
+const userVerify = async (req: Request,
   res: Response,
   next: NextFunction) => {
   const user = (req as AuthRequest).user;
-  if(user?.role !== "user"){
-    return res.status(403).send({message : "Forbidden access"})
+  if (user?.role !== "user") {
+    return res.status(403).send({ message: "Forbidden access" })
   }
 
   next();
 }
 // Admin verify
-const adminVerify = async(req: Request,
+const adminVerify = async (req: Request,
   res: Response,
   next: NextFunction) => {
   const user = (req as AuthRequest).user;
-  if(user?.role !== "admin"){
-    return res.status(403).send({message : "Forbidden access"})
+  if (user?.role !== "admin") {
+    return res.status(403).send({ message: "Forbidden access" })
   }
 
   next();
@@ -95,113 +95,207 @@ export async function connectToMongoDB() {
     const db = client.db("confluxa_db");
     const userCollection = db.collection("user");
     const eventCollection = db.collection("event");
+    const bookingCollection = db.collection("bookings");
 
-    app.get('/users',async(req,res) => {
+    app.get('/users', async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
 
       res.json(result)
     });
 
-    app.post('/events', verifyToken, organizerVerify, async(req,res) => {
-        try {
-            const data = req.body;
+    app.post('/events', verifyToken, organizerVerify, async (req, res) => {
+      try {
+        const data = req.body;
 
-            const result = await eventCollection.insertOne({
-            ...data,
-            status: "pending",
-            rating: 0,
-            reviews: [],
-            createdAt: new Date(),
-            });
+        const result = await eventCollection.insertOne({
+          ...data,
+          status: "pending",
+          rating: 0,
+          reviews: [],
+          createdAt: new Date(),
+        });
 
-            res.json(result);
+        res.json(result);
 
-        } catch (error) {
-            res.status(500).json({
-            message: "Failed to create event",
-            error,
-            });
-        }
+      } catch (error) {
+        res.status(500).json({
+          message: "Failed to create event",
+          error,
+        });
+      }
     })
     // get public ticket 
-    app.get('/event', async (req, res) => {
-      const result = await eventCollection.find().toArray();
+    // app.get('/event', async (req, res) => {
+    //   const result = await eventCollection.find().toArray();
+    //   res.json(result);
+    // });
+
+    // get public event 
+    app.get("/event", async (req, res) => {
+      console.log("QUERY:", req.query);
+      const query: any = {};
+
+      // Only approved events
+      // query.status = "approved";
+
+      // Search by title
+      if (req.query.search) {
+        query.title = {
+          $regex: req.query.search,
+          $options: "i",
+        };
+      }
+      console.log("MONGO QUERY:", query);
+
+      // Category filter
+      if (
+        req.query.category &&
+        req.query.category !== "all"
+      ) {
+        query.category = req.query.category;
+      }
+
+      // Date filter
+      if (req.query.date) {
+        query.date = req.query.date;
+      }
+
+      // Sorting
+      let sortQuery: any = {
+        createdAt: -1,
+      };
+
+      if (req.query.sort === "low") {
+        sortQuery = {
+          price: 1,
+        };
+      }
+
+      if (req.query.sort === "high") {
+        sortQuery = {
+          price: -1,
+        };
+      }
+
+      if (req.query.sort === "rating") {
+        sortQuery = {
+          rating: -1,
+        };
+      }
+
+      // Pagination
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 9;
+
+      const skip = (page - 1) * limit;
+
+      const total = await eventCollection.countDocuments(query);
+
+      const events = await eventCollection
+        .find(query)
+        .sort(sortQuery)
+        .skip(skip)
+        .limit(limit)
+        .toArray();
+
+      res.json({
+        total,
+        totalPages: Math.ceil(total / limit),
+        currentPage: page,
+        events,
+      });
+    });
+
+    // get event by id
+    app.get("/event/:eventId", verifyToken, async (req, res) => {
+      const { eventId } = req.params;
+
+      if (!eventId || Array.isArray(eventId)) {
+        return res.status(400).json({
+          message: "Invalid event id",
+        });
+      }
+
+      const result = await eventCollection.findOne({
+        _id: new ObjectId(eventId),
+      });
+
+      if (!result) {
+        return res.status(404).json({
+          message: "Event not found",
+        });
+      }
+
       res.json(result);
     });
-    
-    // get public ticket 
-    // app.get('/event', async (req, res) => {
-    //   const query = {
-    //       status: "approved",
-    //       hidden: { $ne: true }
-    //   }
 
-    //   // from location 
-    //   if(req.query.from){
-    //     query.fromLocation = {
-    //       $regex: req.query.from.trim(),
-    //       $options: "i",
-    //       // $regex: new RegExp(req.query.from.trim(), "i"),
-    //     }
-    //   }
-    //   // to location 
-    //   if(req.query.to){
-    //     query.toLocation = {
-    //       $regex: req.query.to.trim(),
-    //       $options: "i",
-    //     }
-    //   }
+    // user
+    app.post("/bookings", verifyToken, async (req, res) => {
+      try {
+        const user = (req as any).user;
 
-    //   // transport filter
-    //   if (
-    //     req.query.transport &&
-    //     req.query.transport !== "all"
-    //   ) {
-    //     query.transportType = req.query.transport;
-    //   }
+        const { eventId } = req.body;
 
-    //   let sortQuery = { createdAt: -1 };
+        if (!eventId) {
+          return res.status(400).json({
+            message: "Event id required",
+          });
+        }
 
-    //   // sort by price
-    //   if (req.query.sort === "low") {
-    //     sortQuery = { pricePerUnit: 1};
-    //   }
 
-    //   if (req.query.sort === "high") {
-    //     sortQuery = { pricePerUnit: -1};
-    //   }
-    //   // pagination
+        const existingBooking = await bookingCollection.findOne({
+          eventId,
+          userEmail: user.email,
+        });
 
-    //   // const page = req.query.page || 1;
-    //   // const limit = req.query.limit || 6;
-    //   const page = Number(req.query.page) || 1;
-    //   const limit = Number(req.query.limit) || 9;
 
-    //   const skip = (page - 1) * limit;
+        if (existingBooking) {
+          return res.status(400).json({
+            message: "Already added",
+          });
+        }
 
-    //   const total = await ticketCollection.countDocuments(query);
 
-    //   const tickets = await ticketCollection
-    //     .find(query)
-    //     .sort(sortQuery)
-    //     .skip(skip)
-    //     .limit(limit)
-    //     .toArray();
-    //   // convert string → number here
-    // const formattedTickets = tickets.map(ticket => ({
-    //   ...ticket,
-    //   pricePerUnit: Number(ticket.pricePerUnit),
-    //   ticketQuantity: Number(ticket.ticketQuantity),
-    // }));
-    //   // const result = await ticketCollection.find({  }).toArray();
-    //   res.json({
-    //     total,
-    //     totalPages: Math.ceil(total / limit),
-    //     currentPage: page,
-    //     tickets: formattedTickets,
-    //   });
-    // });
+        const booking = {
+          eventId,
+          userEmail: user.email,
+          createdAt: new Date(),
+        };
+
+
+        const result = await bookingCollection.insertOne(booking);
+
+
+        res.json({
+          success: true,
+          message: "Event added to dashboard",
+          result,
+        });
+
+
+      } catch (error) {
+        res.status(500).json({
+          message: "Something went wrong",
+        });
+      }
+    });
+
+    app.get("/bookings", verifyToken, async(req,res)=>{
+
+      const user = (req as any).user;
+
+
+      const bookings = await bookingCollection
+        .find({
+          userEmail: user.email
+        })
+        .toArray();
+
+
+      res.json(bookings);
+
+    });
 
 
     console.log("You successfully connected to MongoDB!");
@@ -212,7 +306,7 @@ export async function connectToMongoDB() {
 }
 // Call this only when your application terminates
 export async function disconnectFromMongoDB() {
-//   await client.close();
+  //   await client.close();
 }
 
 app.get("/", (req, res) => {
